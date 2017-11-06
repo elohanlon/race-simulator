378001a585e641923c69a3ef02daaa58ea663df7
Eric O <eric@ejdigitalmedia.com>
2017-11-06 12:42:57 -0600

    Part 3 - Add jQuery

--- js/app/main.js
+++ js/app/main.js
@@ -1,4 +1,12 @@
-define(function() {
+define(
+  ['jquery'],
+function($) {
+
+    $(document).ready(function(){
+      console.log("The DOM has loaded!");
+    });
+
     console.log("The main.js requirejs function has executed!");
+
   }
 );
--- js/application.js
+++ js/application.js
@@ -6,6 +6,8 @@ requirejs.config({
     // Note: The paths config is relative to the baseUrl and never includes a
     // ".js" extension because the paths config could be for a directory.
     paths: {
+        // The path 'jquery' will resolve to the jquery library located at lib/jquery.min.js
+        jquery: 'jquery.min',
         app: '../app'
     }
 });
@@ -13,3 +15,10 @@ requirejs.config({
 // Start loading the main app file. Put all of
 // your application logic in there.
 requirejs(['app/main']);
+
+// Set jQuery to noconflict mode so that we make sure to only
+// use it as a require import, and not automatically have it
+// available globally.
+define(['jquery'], function (jq) {
+    return jq.noConflict( true );
+});
